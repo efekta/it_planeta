b119961ea34e92f8417ce7ceee7a57c1fe7c5d21
efekta <efekta9@gmail.com>
2020-11-30 15:31:07 +0300

    start project, header, mob menu, screen 1

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# it_planeta
+<a href="https://efekta.github.io/it_planeta/build/index.html"># it_planeta</a>
---
+++ gulp/config/tasks.js
@@ -0,0 +1,11 @@
+module.exports = [
+    './gulp/tasks/pug',
+    './gulp/tasks/serve',
+    './gulp/tasks/sass',
+    './gulp/tasks/watch',
+    './gulp/tasks/script',
+    './gulp/tasks/img',
+    './gulp/tasks/video',
+    './gulp/tasks/fonts',
+    './gulp/tasks/svg'
+];
