a942b90f07325a4145d27ff848b6af4782463c97
efekta <efekta9@gmail.com>
2020-12-02 15:38:30 +0300

    fix bg lines

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -28,7 +28,14 @@ $(document).ready(function(){
             color : 'f00'
         }
     });
-
+    /*================================
+        Плавный скролл до якоря jquery
+    =================================*/
+    $("body").on('click', '[href*="#"]', function(e){
+        var fixed_offset = 100;
+        $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+        e.preventDefault();
+    });
 
 
 });
